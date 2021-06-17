e54808ba1bdd4862a7f77fc52d7ba182470e2e8e
praiseopara <cpopara2010@yahoo.com>
2021-06-17 12:31:54 -0500

    added features to make
    the user know there are more than 30 issues and giving then the option to route to another page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -11,11 +12,16 @@ var getRepoIssues = function(repo) {
                 response.json().then(function(data) {
                     //pass response data into dom function
                     displayIssues(data);
+
+                    //check if api has paginated issues
+                    if (response.headers.get("Link")) {
+                        displayWarning(repo);
+                    }
                 });
             } else {
                 alert("there was a problem with your request!");
             }
-        });
+        });        
 };
 
 var displayIssues = function(issues) {
@@ -55,4 +61,17 @@ var displayIssues = function(issues) {
     
 };
 
-getRepoIssues("kingopara/git-it-done");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
