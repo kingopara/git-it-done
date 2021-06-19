c100d988e0e3d11f890cad403ec41545a867b95c
praiseopara <cpopara2010@yahoo.com>
2021-06-18 19:23:56 -0500

    linked languages to buttons on the main page. fix 3

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     //format the github api url
@@ -89,6 +90,34 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+                //console.log(data)
+            });
+        } else {
+            alert('Error: GitHub user name not found')
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+    //console.log(language);
+};
+
 //getUserRepos("facebook");
 userFormEl.addEventListener("submit", formSubmitHandler);
 
+languageButtonsEl.addEventListener("click", buttonClickHandler);
